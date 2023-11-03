522ba04196890f7262a7f7275a6703dcbfbb77bb
taren07 <taro.enchi@gmail.com>
2023-11-03 23:45:05 +0900

    Implement selection sort in TypeScript

--- src/sort/selectionSort.ts
+++ src/sort/selectionSort.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!!");
+// SelectionSort
+function selectionSort(arr: number[]): void {
+	const N: number = arr.length;
+	for (let i = 0; i < N - 1; i++) {
+		let minPosition: number = i;
+		let minVal: number = arr[i];
+		for (let j = i + 1; j < N; j++) {
+			if (arr[j] < minVal) {
+				minPosition = j;
+				minVal = arr[j];
+			}
+		}
+		[arr[i], arr[minPosition]] = [arr[minPosition], arr[i]];
+	}
+}
