e98e8efa9825e86075009d451df3707deba37707
taren07 <taro.enchi@gmail.com>
2024-11-18 21:53:29 +0900

    update selectionSort

--- src/sort/selectionSort.go
+++ src/sort/selectionSort.go
@@ -1,19 +1,22 @@
 package sort
 
 // SelectionSort
-func SelectionSort(arr []int) {
-    N := len(arr)
-    for i := 0; i < N-1; i++ {
-        minPosition := i
-        minVal := arr[i]
-        for j := i + 1; j < N; j++ {
-            if arr[j] < minVal {
-                minPosition = j
-                minVal = arr[j]
+func SelectionSort(nums []int) []int {
+    n := len(nums)
+    for i := 0; i < n-1; i++ {
+        minIndex := i
+        for j := i + 1; j < n; j++ {
+            if nums[j] < nums[minIndex] {
+                minIndex = j
             }
         }
-        arr[i], arr[minPosition] = arr[minPosition], arr[i]
+
+        if i != minIndex {
+            nums[i], nums[minIndex] = nums[minIndex], nums[i]
+        }
     }
+
+    return nums
 }
 
 // Please write the main function below.
